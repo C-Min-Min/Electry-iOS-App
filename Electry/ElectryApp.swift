a5dca28f1811c47b7503f5acd814bdf6e1706820
Kaloyan Doychonov <kokolia1070@gmail.com>
2021-07-31 12:07:40 +0300

    [INIT] Initial Commit

---
+++ Electry/ElectryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectryApp.swift
+//  Electry
+//
+//  Created by Kaloyan  Doychinov  on 30.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ElectryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
