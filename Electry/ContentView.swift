94eed3a08d66366e53129f129e7e5380f7febc10
Kaloyan Doychinov <kokolia1070@gmail.com>
2021-08-08 22:39:30 +0300

    [UPDATE] UI

--- Electry/ContentView.swift
+++ Electry/ContentView.swift
@@ -87,8 +87,11 @@ struct ContentView: View {
                             }
                             .frame(maxWidth: .infinity)
                             .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
-                            .background(Color.accentColor)
+                            .background(
+                                Rectangle().fill(((Color.secondary).opacity(0.2)))
+                            )
                             .cornerRadius(15)
+
                         }
                     }
                     .padding(15)
@@ -104,18 +107,30 @@ struct ContentView: View {
             
             NavigationView{
                 ScrollView{
-                    LazyVGrid(columns: glayout, spacing: 100, content: {
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                        Text("Test")
-                    })
+                    LazyVGrid(columns: glayout, spacing: 15){
+                        ForEach(devices, id: \.self){ device in
+                            VStack(alignment: .leading, spacing: 2.5){
+                                Image(systemName: device["image_link"]!)
+                                Spacer()
+                                Text(device["dev_name"]!)
+                                    .font(.system(size: 20))
+                                    .fontWeight(.bold)
+                                Spacer()
+                                Text(device["status"] ?? "Unknown")
+                                    .font(.body)
+                                    .bold()
+                                    
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
+                            .background(
+                                Rectangle().fill(((Color.secondary).opacity(0.2)))
+                            )
+                            .cornerRadius(15)
+
+                        }
+                    }
+                    .padding(15)
                 }
                 .navigationBarTitle("Devices")
             }
